a5bbfa2bbc37dc9cc35e4b23259aa45fae318638
angelchavez19 <infoangelchavez@gmail.com>
2025-08-02 15:50:42 -0500

    feat(style): set default css variables

--- app/components/sample.vue
+++ app/components/sample.vue
@@ -8,10 +8,14 @@ defineProps<Props>();
 
 <template>
   <main
-    class="flex flex-col items-center justify-center min-h-screen bg-gray-100"
+    class="flex flex-col items-center justify-center min-h-screen bg-(--gs-c-bg) text-(--gs-c-text-1)"
   >
-    <h1 class="text-2xl font-bold">{{ title }}</h1>
-    <p class="mt-2 text-gray-600">{{ description }}</p>
+    <h1 class="text-2xl font-bold">
+      {{ title }}
+    </h1>
+    <p class="mt-2 text-(--gs-c-text-3)">
+      {{ description }}
+    </p>
 
     <slot />
   </main>
